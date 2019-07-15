c17effb5eefc08e553a2ed812fb4bb70f039798c
gaoxiuhui <52287241+gaoxiuhui@users.noreply.github.com>
2019-07-15 11:14:48 +0800

    Create idnex.js

---
+++ idnex.js
@@ -0,0 +1,14 @@
+var user=new Object;
+user.name='John';
+user.surname='Mike';
+user.name='Peter';
+user.name='';
+console.log(user)
+
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+};
+var  count=fruit.apple+fruit.peach+fruit.pear;
+alert(count);
